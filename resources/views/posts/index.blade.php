01abf16f0af34051b40aff05036851fa937876a9
jinmy <solisjinmy@gmail.com>
2017-03-06 00:56:50 +0000

    primer commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.myapp')
+
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+    <h2>Title</h2>
+    <p>Poster 5 hour ago</p>
+    </div>
+ <hr>
+   <div class="col-md-12">
+    <h2>Title</h2>
+    <p>Poster 5 hour ago</p>
+    </div>
+  <hr>  
+    <div class="col-md-12">
+    <h2>Title</h2>
+    <p>Poster 5 hour ago</p>
+    </div>
+
+</div>
+
+
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.myapp')
+
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+    <h2>Title</h2>
+    <p>Descripcion</p>
+    <p>Poster 5 hour ago</p>
+    </div>
+ 
+
+</div>
+
+
+@endsection
